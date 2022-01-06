51bba64be8dfc8cad611642e5a2e54cfcff0329a
Fernando de Almeida <fernandoa.code@gmail.com>
2022-01-05 23:26:07 -0300

    config babel config es2015 export import

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
     entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader: 'babel'
+        }]
     }
 }
